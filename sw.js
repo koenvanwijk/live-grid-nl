455f71f74757126eec0795d650c2bec6ca00f160
koenvanwijk <koenvanwijk@users.noreply.github.com>
2026-08-10 21:09:08 +0200

    Refresh PWA cache for built-in station LOD

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const STATIC_CACHE='nl-grid-static-v8';
-const RUNTIME_CACHE='nl-grid-runtime-v8';
-const STATIC_ASSETS=['./','./index.html','./styles.css?v=11','./app.js?v=10','./data/interconnectors.js?v=2','./data/injections.js?v=3','./data/interconnector-flow.js?v=1','./data/flow-particles.js?v=4','./data/performance.js?v=1','./manifest.webmanifest','./icon.svg'];
+const STATIC_CACHE='nl-grid-static-v9';
+const RUNTIME_CACHE='nl-grid-runtime-v9';
+const STATIC_ASSETS=['./','./index.html','./styles.css?v=11','./app.js?v=11','./data/interconnectors.js?v=3','./data/injections.js?v=3','./data/interconnector-flow.js?v=1','./data/flow-particles.js?v=4','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(STATIC_CACHE).then(cache=>cache.addAll(STATIC_ASSETS)).then(()=>self.skipWaiting()));
